ad5701b3be5a13272166fd1c598a6bf622ff2518
Maenda Fortier <maenda@pixelunion.net>
2020-04-21 11:17:14 -0400

    Convert accordion json to spaces

--- widget-templates/accordion.js
+++ widget-templates/accordion.js
@@ -1,40 +1,40 @@
 module.exports = {
   "name": "Accordion",
-	"schema": [
+  "schema": [
     {
-			"type": "array",
-			"label": "Accordion",
-			"id": "accordionTabs",
-			"defaultCount": 3,
-			"entryLabel": "Tab",
-			"schema": [
-				{
-					"type": "tab",
-					"label": "Content",
+      "type": "array",
+      "label": "Accordion",
+      "id": "accordionTabs",
+      "defaultCount": 3,
+      "entryLabel": "Tab",
+      "schema": [
+        {
+          "type": "tab",
+          "label": "Content",
           "sections": [
             {
               "settings": [
-               	{
-									"type": "input",
-									"label": "Title",
-									"id": "title",
-									"default": "Title"
-              	},
-        				{
-									"type": "code",
-									"label": "Content",
-									"id": "content",
-									"typeMeta": {
-										"placeholder": "<div>Insert content here</div>",
-										"language": "html"
-									}
-								}
+                 {
+                  "type": "input",
+                  "label": "Title",
+                  "id": "title",
+                  "default": "Title"
+                },
+                {
+                  "type": "code",
+                  "label": "Content",
+                  "id": "content",
+                  "typeMeta": {
+                    "placeholder": "<div>Insert content here</div>",
+                    "language": "html"
+                  }
+                }
               ]
             }
           ]
-				}
-			]
-		}
-	],
+        }
+      ]
+    }
+  ],
   "template": "{{#each accordionTabs}}<h1>title:{{title}}</h1><div>content:{{{content}}}</div>{{/each}}"
 }
